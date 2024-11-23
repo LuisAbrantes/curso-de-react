f8b149c7811bffce39c5f32d3d3f059122156385
Luis Henrique dos Santos Abrantes <luis.hsa@gmail.com>
2024-11-23 11:58:27 -0300

    Add Prettier configuration, enhance AddTask and Tasks components, and update package.json scripts and dependencies

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,21 @@
-function AddTask(){
-    return <h1>AddTask</h1>;
+function AddTask() {
+    return (
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input
+                type="text"
+                placeholder="Type the task title"
+                className="border border-slate-300 outline-state-400 px-4 py-2 rounded-md"
+            />
+            <input
+                type="text"
+                placeholder="Type the task description"
+                className="border border-slate-300 outline-state-400 px-4 py-2 rounded-md"
+            />
+            <button className="bg-slate-500 px-4 text-white py-2 rounded-md font-medium">
+                Add
+            </button>
+        </div>
+    );
 }
 
 export default AddTask;
